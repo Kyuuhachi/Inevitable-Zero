50dab911a917e8f7055f0c7e2902e4e4fa3e26bd
Kyuuhachi <caagr98@gmail.com>
2020-08-18 14:24:06 +0200

    Use only standard libraries

--- scena.py
+++ scena.py
@@ -1,9 +1,9 @@
-import kaiseki
-
-import main
 import argparse
 argp = argparse.ArgumentParser()
-argp.add_argument("file", type=argparse.FileType("rb"))
-@main(argp=argp)
+argp.add_argument("source", type=argparse.FileType("rb"))
+
 def __main__(source):
 	print(source)
+
+if __name__ == "__main__":
+	__main__(**argp.parse_args().__dict__)
